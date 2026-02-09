a2e1ae8ba6a913a3d9e5a3b416f3d3563e255897
teokontos <romaios99@gmail.com>
2026-02-09 02:01:24 +0200

    Update extract_station.py

--- .github/workflows/extract_station.py
+++ .github/workflows/extract_station.py
@@ -17,7 +17,6 @@ from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 
 service = Service(ChromeDriverManager().install())
-driver = webdriver.Chrome(service=service, options=options)
 options = webdriver.ChromeOptions()
 options.add_argument("--headless=new")
 options.add_argument("--no-sandbox")
